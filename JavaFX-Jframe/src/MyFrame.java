c25a657d5320b7ab99e1c1365938c5a502755466
Arman Hossain <hossain4arman@gmail.com>
2025-09-15 13:09:59 +0600

    Uploading First Tutorials

---
+++ JavaFX-Jframe/src/MyFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    public MyFrame() {
+        this.setTitle("First Java App");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(720, 720) ;
+        this.setVisible(true); // Make frame visible
+
+        ImageIcon image = new ImageIcon("src/favicon.png") ;
+        this.setIconImage(image.getImage()); // change the icon of the frmae
+        //frame.getContentPane().setBackground(Color.green); //change the background color
+        this.getContentPane().setBackground(new Color(123,50,250)); // custom color
+    }
+}
